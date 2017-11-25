0578f0b14f7431c341eb34036bd86e1ebc13f1a5
moherril <moherril@MB123WS132.WIN.ecf.utoronto.ca>
2017-11-25 14:47:33 -0500

    updated xml with RatingController, implemented RatingController functionality, modified RatingDao

--- MIE350 Project V2/src/com/mie/dao/RatingDao.java
+++ MIE350 Project V2/src/com/mie/dao/RatingDao.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 import com.mie.model.Menu;
 import com.mie.model.Restaurant;
 import com.mie.model.StudentUser;
+import com.mie.model.User;
 import com.mie.util.DbUtil;
 
 public class RatingDao {
@@ -23,7 +24,24 @@ public class RatingDao {
 		 */
 		connection = DbUtil.getConnection();
 	}
-
+	
+	public boolean isValid(User user, int restaurantID) {
+		boolean result = false;
+		try {
+			String email = user.getEmail();
+			//check if the rating exists
+			String query = "SELECT * FROM Rating WHERE email = " + email + " AND RestaurantID = " + restaurantID;
+			Statement stmt;
+			stmt = connection.createStatement();
+			stmt.execute(query);
+			ResultSet rs = stmt.executeQuery(query);
+			result = rs.next();
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return result;
+	}
+	
 	public void addRating(StudentUser student, Restaurant res, int rating) {
 		/**
 		 * This method adds a new rating to the database
@@ -48,23 +66,7 @@ public class RatingDao {
 		}
 	}
 	
-	public void changeRating(StudentUser student, Restaurant res, int rating){
-		/**
-		 * This method changes an existing rating in the database
-		 */
-		try {
-			int studentID = student.getUserID();
-			int resID = res.getResID();
-	
-			String query = "UPDATE Rating SET Rating = " + rating +" WHERE UserID = " + studentID + " AND RestaurantID = " + resID + ";";
-			Statement stmt = connection.createStatement();
-			stmt.execute(query);
-			
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-	}
-	
+
 	public int getRating(StudentUser student, Restaurant res, int rating){
 		/**
 		 * This method retrieves existing rating a user did on a restaurant
@@ -105,4 +107,6 @@ public class RatingDao {
 			e.printStackTrace();
 		}	
 	}
+
+
 }
